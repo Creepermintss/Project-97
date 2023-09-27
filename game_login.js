a72b6581aeaf8ff61a306b31e2a1d1dee1eff3bf
Creepermintss <144182795+Creepermintss@users.noreply.github.com>
2023-09-26 20:03:45 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+// Create function addUser()
+function id addUser() {
+  // Get value of user by id player1_name_input and player2_name_input
+  value = id player1_name_input and player2_name_input;
+  // Store these values locally
+  game_page.html = window.location;
+  //Assign "game_page.html" to window.location
+}
+
